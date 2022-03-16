3db0c5c21609b683f3dad08791304abea00e6730
danielrb97 <60640897+danielrb97@users.noreply.github.com>
2022-03-16 00:25:44 -0600

    creacion del ejecudador de sonar

---
+++ cidr_convert_api/go/sonar.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=Final-project1 \
+  -Dsonar.sources= \
+  -Dsonar.host.url=http://ec2-18-188-196-47.us-east-2.compute.amazonaws.com:9000 \
+  -Dsonar.login=71578fd0cb7dcf663c19a6eec38f0a00e8986ba4
